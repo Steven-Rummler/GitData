a89bbb48cd920bd1788a9d36a45f817903b4fee1
Steven Rummler <steven.rummler@gmail.com>
2022-12-26 18:28:55 -0700

    update

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import subprocess
 
 out = subprocess.check_output(
-    'git -C ../steven-rummler.github.io log --no-merges --stat=100000,8192')
+    'git -C ../steven-rummler.github.io log --no-merges --numstat')
 
 lines = out.decode('ascii').split('\n')
 
